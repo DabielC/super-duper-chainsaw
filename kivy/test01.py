272767b8489048ed15a6a43fdcbce6e947a4bac7
DanielC <105137974+DabielC@users.noreply.github.com>
2022-11-14 09:03:12 +0700

    Add files via upload

--- kivy/test01.py
+++ kivy/test01.py
@@ -2,10 +2,9 @@ import random
 import csv
 from googletrans import Translator
 
-"""translator = Translator()
+translator = Translator()
 
-translated_text = translator.translate('Mhong', dest='th')
-print(translated_text.text)"""
+translated_text = translator.translate('pan', dest='th')
 
 temp = []
 Name = []
@@ -19,14 +18,16 @@ with open('ez.csv', 'r', newline='', encoding="latin") as mycsv:
 user = int(input())
 for i in range(user):
 	a = random.choice(temp)
-	Name.append(a[0])
+	Name.append(str(a[0]).lower().rstrip())
 	temp.remove(a)
 
-
 def guesser(Name):
-	quest = list(random.choice(Name))
+	ran = random.choice(Name)
+	trans = translator.translate('%s' %(ran), dest='th')
+	print(trans.text)
+	quest = list(ran)
 	Name.remove(''.join(quest))
-	times = random.randint(2,len(quest)-2)
+	times = random.randint(2,len(quest)-1)
 
 
 	for i in range(times):
@@ -36,17 +37,21 @@ def guesser(Name):
 		elif quest[index_quest] == ' ':
 			times += 1
 
-	return ''.join(quest)
+	return ''.join(quest), ran
 
 sec = len(Name)
 score = 0
 name = Name.copy()
 
 for i in range(sec):
-	print(guesser(name))
-	ans = input()
+	a, b = guesser(name)
+	print(a)
+	ans = input().lower()
 	if ans in Name:
 		score += 1
+		print(b)
+	else:
+		print(b)
 	sec -= 1
 
 print("%d / %d" %(score, len(Name)))
